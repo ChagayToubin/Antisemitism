a734b55d10a384a7262827a2957c55cfb7d6fe05
chagay <chagaytoubin@gmail.com>
2025-08-04 12:48:04 +0300

    finish find 3 most length 0 1

--- src/Controler.py
+++ src/Controler.py
@@ -16,11 +16,16 @@ class control:
 
     def research_control(self):
 
-        count_0_1=dataAnalyzer.count_1_0(self.df_copy)
-        print(count_0_1)
+        # count_0_1=dataAnalyzer.count_1_0(self.df_copy)
+        # print(count_0_1)
+        #
+        # count_0_1_len_words=dataAnalyzer.count_0_1_len_sentence(self.df_copy)
+        # print(count_0_1_len_words)
+        # count_0_1_find_3_biggest_tweets=dataAnalyzer.count_0_1_find_3_biggest_tweets(self.df_copy)
+        # print(count_0_1_find_3_biggest_tweets)
+        most_common_words=dataAnalyzer.most_common_words(self.df_copy)
+        print(most_common_words)
 
-        count_0_1_len_words=dataAnalyzer.count_0_1_len_sentence(self.df_copy)
-        print(count_0_1_len_words)
 
 
 
